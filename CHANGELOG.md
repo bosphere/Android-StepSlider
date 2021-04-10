a95a4e2a6b95a8d819a0fdce94634ea163834c53
bosphere <boss.army@gmail.com>
2021-04-10 17:14:07 +0800

    v1.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,11 @@
 Change Log
 ============================
 
+## Version 1.0.3
+_2021-04-10_
++ AndroidX migration
++ Publish to Maven Central
+
 ## Version 1.0.2
 _2018-03-22_
 + Support DPAD on non-touchable device i.e. Android TV
--- app/src/main/java/com/bosphere/stepsliderdemo/MainActivity.java
+++ app/src/main/java/com/bosphere/stepsliderdemo/MainActivity.java
@@ -1,6 +1,6 @@
 package com.bosphere.stepsliderdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.bosphere.stepslider.OnSliderPositionChangeListener;
